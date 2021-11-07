0af60d01a94da440e74a0cb2b0b9295f9a3392e6
ale-river <alejandra.rvrs@gmail.com>
2021-11-07 15:51:53 -0600

    scraping

--- scraping.py
+++ scraping.py
@@ -21,7 +21,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemisphere(browser)
     }
 
     # Stop webdriver and return data
